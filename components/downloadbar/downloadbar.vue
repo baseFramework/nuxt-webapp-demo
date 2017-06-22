aa7138130fcb7fd49bd668560e3e6885de610217
wurh <ro87630872@126.com>
2017-06-22 14:15:51 +0800

    [INI] 初始化工程

---
+++ components/downloadbar/downloadbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="J-download-bar" style="display: block !important;">
+    <div class="top-download-bar J-download-bar" style="display: block !important;">
+      <a class="download-link J-download-link" mars_sead="index_top_client_download_btn" href="//viva.vip.com/s/mdl"
+         style="display: block !important;">
+        <div class="download-text" style="display: block !important;">
+          <p class="text" style="display: block !important;">新会员满￥88元包邮</p>
+          <p class="sub-text" style="display: block !important;">现在下载app，立享优惠</p>
+        </div>
+      </a>
+      <a href="javascript:;" class="J-download-close download-close icon-close" mars_sead="close_download_page"
+         style="display: block !important;"></a>
+    </div>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'cdownload',
+  }
+
+
+</script>
+
+
+<style scoped>
+  @import "donwloadbar.css";
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+    <section>
+        <my-downloads></my-downloads>
+        <div class="container">
+            <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+            <h1 class="title">
+                USERS
+            </h1>
+            <ul class="users">
+                <li v-for="(user, index) in users" class="user">
+                    <nuxt-link :to="{ name: 'id', params: { id: index }}">
+                        {{ user.name }}
+                    </nuxt-link>
+                </li>
+            </ul>
+        </div>
+    </section>
+</template>
+
+<script>
+import axios from '~plugins/axios'
+import MyDownloads from '../components/downloadbar/downloadbar.vue'
+
+export default {
+    components: {
+        MyDownloads
+    },
+  async asyncData () {
+    let { data } = await axios.get('/api/users')
+    return {
+      users: data
+    }
+  },
+  head () {
+    return {
+      title: 'Users'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.title
+{
+  margin: 30px 0;
+}
+.users
+{
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+.user
+{
+  margin: 10px 0;
+}
+</style>
