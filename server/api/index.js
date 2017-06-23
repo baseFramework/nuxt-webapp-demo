83c130bf81e0af0ebfa91580dd2deae83f41e0aa
wurh <ro87630872@126.com>
2017-06-23 11:50:50 +0800

    [UPD] 更新组件

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,12 @@
 import { Router } from 'express'
 
 import users from './users'
+import operates from './operates'
 
 var router = Router()
 
 // Add USERS Routes
 router.use(users)
+router.use(operates)
 
 export default router
---
+++ server/api/operates.js
@@ -0,0 +1,28 @@
+import { Router } from 'express'
+
+var router = Router()
+
+// Mock Users
+// const users = [
+//     { name: 'Alexandre' },
+//     { name: 'Sébastien' }
+// ]
+
+const operate1 = require('../data/operates1.json')
+
+/* GET users listing. */
+router.get('/operate1', function (req, res, next) {
+    res.json(operate1)
+})
+//
+// /* GET user by ID. */
+// router.get('/users/:id', function (req, res, next) {
+//     var id = parseInt(req.params.id)
+//     if (id >= 0 && id < users.length) {
+//         res.json(users[id])
+//     } else {
+//         res.sendStatus(404)
+//     }
+// })
+
+export default router
